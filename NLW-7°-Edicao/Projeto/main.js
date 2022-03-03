26c15be59fac4dc9c306876f038b73b9c60b2dfd
Lucas Araújo <80704314+LFeli@users.noreply.github.com>
2022-03-02 22:17:36 -0300

    Add files via upload

---
+++ NLW-7°-Edicao/Projeto/main.js
@@ -0,0 +1,37 @@
+
+
+const linksSocialMedia = {
+   gituhub: 'Lfeli',
+   youtube: '',
+   facebook: '',
+   instagram: '',
+   twitter: 'wisezito'
+}
+
+function changeSocialMediaLinks() {
+
+   for(let li of socialLinks.children){
+      const social = li.getAttribute('class');
+
+      li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+   }
+}
+
+changeSocialMediaLinks();
+
+function getGithubProfileInfos(){
+   const url = `https://api.github.com/users/${linksSocialMedia.gituhub}`
+
+   fetch(url)
+   .then(response => response.json())
+   .then(data => {
+      userAvatar.src = data.avatar_url;
+      userName.textContent = data.name;
+      userGithub.href = data.html_url;
+      userDescripition.textContent = data.bio;
+      userLogin.textContent = data.login;
+
+   })
+}
+
+getGithubProfileInfos();
